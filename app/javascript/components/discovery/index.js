602aa3982680228ad0740fb252ef2d74a82218fa
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-04-20 21:54:59 -0300

    Chamando API no component Discovery

--- app/javascript/components/discovery/index.js
+++ app/javascript/components/discovery/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Columns, Heading } from "react-bulma-components";
 import styled from "styled-components";
+import AlbumsService from "../../services/album";
 import Album from "../album";
 
 const DivVSpaced = styled.div`
@@ -8,38 +9,20 @@ const DivVSpaced = styled.div`
 `;
 
 const Discovery = () => {
-  const albums_mock = [
-    {
-      id: 1,
-      artist_name: "Andrew Howes",
-      title: "Gubernator",
-      cover_url:
-        "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2f248b465e87a8800d0362b55a3b16f9d7173888/gubernator.jpg",
-    },
-    {
-      id: 2,
-      artist_name: "Andrew Howes",
-      title: "The Great Bear",
-      cover_url:
-        "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ffbb0eea38171a0cd8c85f977e3bf2e4f9996775/the_great_bear.jpg",
-    },
-    {
-      id: 3,
-      artist_name: "Yellow Chair",
-      title: "Barcelona",
-      cover_url:
-        "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBGUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--b56f4c80ed66afda06173d0530bbf8c720ad7ae0/barcelona.jpg",
-    },
-    {
-      id: 4,
-      artist_name: "Andrew Howes",
-      title: "Gubernator",
-      cover_url:
-        "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2f248b465e87a8800d0362b55a3b16f9d7173888/gubernator.jpg",
-    },
-  ];
+  const [recentAlbums, setRecentAlbums] = useState([]);
+  const [recommendedAlbums, setRecommendedAlbums] = useState([]);
 
-  const albums = albums_mock.map((album, key) => (
+  async function fetchAlbums() {
+    const response = await AlbumsService.index();
+    setRecentAlbums(response.data.recent_albums);
+    setRecommendedAlbums(response.data.recommend_albums);
+  }
+
+  useEffect(() => {
+    fetchAlbums();
+  }, []);
+
+  const recentAlbumsComponents = recentAlbums.map((album, key) => (
     <Columns.Column desktop={{ size: 3 }} mobile={{ size: 6 }} key={key}>
       <Album
         artist_name={album.artist_name}
@@ -50,21 +33,37 @@ const Discovery = () => {
     </Columns.Column>
   ));
 
+  const recommendedAlbumsComponents = recommendedAlbums.map((album, key) => (
+    <Columns.Column desktop={{ size: 3 }} mobile={{ size: 6 }} key={key}>
+      <Album
+        artist_name={album.artist_name}
+        title={album.title}
+        cover_url={album.cover_url}
+        key={key}
+        id={album.id}
+      />
+    </Columns.Column>
+  ));
+
   return (
     <>
-      <div>
-        <Heading className="has-text-white" size={4}>
-          Tocadas recentemente
-        </Heading>
-        <Columns className="is-mobile">{albums}</Columns>
-      </div>
+      {recentAlbumsComponents.length > 0 && (
+        <div>
+          <Heading className="has-text-white" size={4}>
+            Tocadas recentemente
+          </Heading>
+          <Columns className="is-mobile">{recentAlbumsComponents}</Columns>
+        </div>
+      )}
 
-      <DivVSpaced>
-        <Heading className="has-text-white" size={4}>
-          Recomendadas
-        </Heading>
-        <Columns className="is-mobile">{albums}</Columns>
-      </DivVSpaced>
+      {recommendedAlbumsComponents.length > 0 && (
+        <DivVSpaced>
+          <Heading className="has-text-white" size={4}>
+            Recomendadas
+          </Heading>
+          <Columns className="is-mobile">{recommendedAlbumsComponents}</Columns>
+        </DivVSpaced>
+      )}
     </>
   );
 };
--- app/javascript/services/album.js
+++ app/javascript/services/album.js
@@ -1,4 +1,4 @@
-import Api from ".api";
+import Api from "./api";
 
 const AlbumsService = {
   index: () => Api.get("/dashboard"),
